57f645e6334b4357e148a4f0657ab17aae58d46d
HYEBA <aslk3366zz@naver.com>
2021-03-22 19:39:27 +0900

    방 데이터 표현 클래스 추가

---
+++ app/src/main/java/org/jangtechtown/study_20210322/datas/Room.kt
@@ -0,0 +1,10 @@
+package org.jangtechtown.study_20210322.datas
+
+//패키지에 클래스 추가 후 클래스에 정보를 받아올 값과 자료형 입력
+
+class Room(
+    val price : Int,
+    val address : String,
+    val floor : Int,
+    val description : String) {
+}
